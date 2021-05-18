f52df29cac953870b31f3a100c2ae29fc0832f09
IsaacMendez-work <73685690+IsaacMendez-work@users.noreply.github.com>
2021-05-17 21:01:50 -0400

    first

---
+++ src/main/java/com/tts/demo/W13D4Application.java
@@ -0,0 +1,14 @@
+package com.tts.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class W13D4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W13D4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/demo/W13D4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W13D4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
